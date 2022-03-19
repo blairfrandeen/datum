e5d98749377474d889c8ad2397b7fb822ae9fed0
Blair Frandeen <Blair.S.Frandeen@jpl.nasa.gov>
2022-03-19 10:19:29 -0700

    CLI Improvements for populate named ranges
    Modified xl_populate_named_ranges.py to improve the user experience.
    Rather than manually entering file names in the python script, the user
    can now select from open workbooks that are seen by `xlwings`, and JSON
    files in the users current working directory.
    
    Additionally, the named ranges to be updated are listed with the old and
    new proposed values, as well as the percent that these values change.
    The user is prompted to confirm prior to overwriting values. The intent
    of this update is to eliminate errors & go-backs.

--- xl_populate_named_ranges.py
+++ xl_populate_named_ranges.py
@@ -59,7 +59,7 @@ def write_named_range(workbook, range_name, new_value):
     rng = xw_get_named_range(workbook, range_name)
     if rng is None: return None
 
-    rng.value = new_value * 1000
+    rng.value = new_value
 
 def backup_workbook(workbook):
     """Create a backup copy of the workbook. Returns
@@ -82,7 +82,7 @@ def backup_workbook(workbook):
     return new_workbook
 
 
-def update_named_ranges(json_file, excel_file, backup=True):
+def update_named_ranges(json_file, workbook, backup=True):
     """
     Open a JSON file and an excel file. Update the named
     ranges in the excel file with the corresponding JSON values.
@@ -92,9 +92,6 @@ def update_named_ranges(json_file, excel_file, backup=True):
     of type "area", along with other expressions. To populate this in 
     Excel, we need to name the range "SURFACE_SPHERICAL.area"
     """
-    workbook = xw_get_workbook(excel_file)
-    if backup:
-        workbook = backup_workbook(workbook)
 
     # make a dict of named ranges, measurement names, and measurement types
     workbook_named_ranges = dict()
@@ -106,8 +103,12 @@ def update_named_ranges(json_file, excel_file, backup=True):
             measurement_type = None
         workbook_named_ranges[measurement_name] = measurement_type
     # print(workbook_named_ranges)
+    write_list = dict()
     with open(json_file, "r") as json_file_handle:
         json_data = json.load(json_file_handle)
+        #TODO: Add argument to function to skip confirmation
+        print('{0:<32} {1:>12} {2:>12} {3:>15}'.format('NAME', 'OLD VALUE', 'NEW VALUE', 'PERCENT CHANGE'))
+        print('{0:<32} {1:>12} {2:>12} {3:>15}'.format('------------', '------------', '------------', '------------'))
         for measurement in json_data['measurements']:
             if measurement['name'] in workbook_named_ranges.keys():
                 range_name = measurement['name']
@@ -125,19 +126,81 @@ def update_named_ranges(json_file, excel_file, backup=True):
                     measurement_type = workbook_named_ranges[measurement['name']]
                 # print the value currently in Excel
                 excel_value = read_named_range(workbook, range_name)
-                print(f'Excel value of {range_name}: {excel_value}')
+                # print(f'Excel value of {range_name}: {excel_value}')
                 # print the value currently in JSON
-                print(f'JSON Value: {json_value}')
-                write_named_range(workbook, range_name, json_value)
+                # print(f'JSON Value: {json_value}')
+                percent_change = (json_value - excel_value) / excel_value * 100
+                print('{0:<32} {1:>12.5} {2:>12.5} {3:>14.3}%'.format(range_name, excel_value, json_value, percent_change))
+                write_list[range_name] = json_value
+
+    print("The values listed above will be overwritten. Enter 'y' to continue: ", end='')
+    overwrite_confirm = input()
+    if overwrite_confirm == 'y':
+        if backup:
+            workbook = backup_workbook(workbook)
+        for range in write_list.keys():
+            write_named_range(workbook, range, write_list[range])
+    else:
+        print("Aborted.")
+
+def user_select_item(item_list, item_type):
+    """Given a list of files or workbooks, enumerate them and
+    ask the user to select one item.
+    
+    Return the index of the selected item."""
+    # list the items
+    print(f'Available {item_type}s:')
+    for index, element in enumerate(item_list):
+        print(f'[{index}] - {element}')
+    while True: # keep asking for input until a valid input or quit command received
+        print(f'Select {item_type} index (q to quit): ', end='')
+        selection_index = input()
+        if selection_index == 'q':
+            return None
+        try:
+            selection_index = int(selection_index)
+        except ValueError: # if selection is non-integer
+            print('Invalid input.')
+            continue
+        if selection_index >= len(item_list) or selection_index < 0:
+            print('Index out of bounds.')
+            continue
+        else:
+            return selection_index
+
+def user_select_open_workbook():
+    workbook_list = [ book.name for book in xw.apps[0].books ]
+    if len(workbook_list) > 0:
+        workbook_index = user_select_item(workbook_list, 'Excel Workbook')
+        if workbook_index is not None:
+            return xw.apps[0].books[workbook_index]
+        else:
+            return None
+    else:
+        print("No open workbooks detected.")
+        return None
+
+def user_select_json_file():
+    json_file_list = []
+    for file in os.listdir():
+        if file.endswith('.json'):
+            json_file_list.append(file)
+    if len(json_file_list) > 0:
+        json_index = user_select_item(json_file_list, 'JSON file')
+        if json_index is not None:
+            #TODO: Make this compatible with linux/macOS - use pathlib?
+            json_file_path = f'{os.getcwd()}\\{json_file_list[json_index]}'
+            return json_file_path
+        else:
+            return None
+    else:
+        print("No JSON files detected in working directory.")
+        return None
 
 def main():
-    # wb = xw_get_workbook("named_range_test.xlsx")
-    json_file = "C:\\Users\\frandeen\\Documents\\datum\\json_test.json"
-    # update_named_ranges(json_file, "Surface Coating Usable Area.xlsx")
-    # update_named_ranges(json_file, "OS Mass Threats & Opportunities.xlsx")
-    update_named_ranges(json_file, "named_range_test.xlsx")
-    # for named_range in wb.names:
-    #     print(f'{named_range.name} = {read_named_range(wb, named_range.name)}')
+    json_file = user_select_json_file()
+    excel_workbook = user_select_open_workbook()
+    update_named_ranges(json_file, excel_workbook)
 
 if __name__ == '__main__':
     main()
